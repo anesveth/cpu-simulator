10f0e6024fd002cdc9ea3730a6ae52e99c7c6ce2
Jean <jpmejicanos@ufm.edu>
2019-08-14 16:04:15 -0600

    Added the code reader and CU base, Modified the IC atributes and created a file for opcode instructions

--- CPU.py
+++ CPU.py
@@ -1,68 +1,20 @@
-#Grupo 7
-
-#Cosas Importantes en texto --------------------------------------------
-# Recuerdense de poner todas las funciones que puedan en FCO. 
-# Este solo es un medio layout, no quiere decir que esos sean los attributos que vamos a 
-# usar, tampoco son los correctos (solo lo hize rapido sin leer exactamente lo que queria el prof en las instrucciones)
-# Tambien recuerdense de usar CI asi sacamos puntos extras. Todos los demas puntos extras deberiamos integrar
-# mas tarde ya que son mas dificiles.
-
-
-
-class IC:
-    def __init__(self, manufacterer, build_date, purpose):
-        self.manufacterer  = manufacterer
-        self.build_date = build_date
-        self.purpose = purpose
-
-class Memory:
-    def __init__(self, algo, algo2):
-        self.algo = algo
-        self.algo2 = algo2
-
-class ALU(IC):
-    def __init__(self, zero, overflow, negative, input2, output): #Hay que cambiarlos a flags. Ejemplo de if commands para los flags abajo.
-        self.zero = zero
-        self.overflow = overflow
-        self.negative = negative
-        self.input = input2
-        self.output = output
-
-class CU(IC):
-    def __init__(self, saber):
-        self.saber = saber
-    
-class Registers:
-    def __init__(self, a, b, c, d, pc, ir, or2):
-        self.a = a
-        self.b = b
-        self.c = c
-        self.d = d
-        self.pc = pc
-        self.ir = ir
-        self.or2 = or2
-
-class RAM:
-    def __init__(self, x):
-        self.x = x
-
-class Clock:
-    frequency = 1.2 #Hay que poner el range de 0 a 2
-
-# when ENTER: #Ni idea como escribir esto en code. Talvez algo como input("Press Enter to skip to next step: ") y aparte el metodo.
-#    step == TRUE
-
-# if step == TRUE:
-#    frequency = 0
-    
-# if op == -x:
-#    negmethod()
-
-# if op > 15:
-#    ovmethod()
-
-# if op == 0:
-#     0method()
-
-
+#fk control unit
+from IC import IC
+import Reader
+class cu(IC):
+    def recibe_code(self):
+        #pass
+        print("")
+    def interpretacion(self):
+        #pass
+        print("ARRREEE")
+    def main_thread(self):
+        '''controls and connects everything'''
+        hard_drive = Reader.CardReader()
+        print("Ingresa nombre del codigo (sin el .code)")
+        file_name = input()
+        hard_drive.change_file(file_name)
+        intruction = hard_drive.read_line()
+        #decides what to do with instructions
+        
 
---
+++ Reader.py
@@ -0,0 +1,76 @@
+from os.path import isfile as file_exist
+from IC import IC #Importing SuperClass
+class CardReader(IC):
+    '''Manages exterior files'''
+    #other bouilder is already passed down by the father
+    def __init__(self):
+        self.file = ""
+
+    def check_code(self):
+        def check_binary(bistring):
+            '''checks if a string is binary'''
+            for char in bistring:
+                if (char == 1 or char == 0):
+                    return False
+            return True
+        '''Checks code in file, returns 0 if file doesnt exist'''
+        #chequeamos existencia del archivo  
+        if (self.file != ""):
+            self.file.seek(0)
+            #revisamos
+            line = 1
+            current_line = self.file.readline()
+            errors = ""
+            while (current_line != ""):
+                size = len(current_line) == 10
+                space = current_line.count(" ") == 1
+                tem = current_line.replace(" ", "").replace("\n", "")
+                digit = check_binary(tem)
+                print(size)
+                print(space)
+                print(tem)
+                print (digit)
+                #print(current_line.count(" ") == 1 and len(current_line) == 10 and current_line.replace(" ", "").isdigit())
+                if (not(size and space and digit)):
+                    #has an error
+                    errors = errors +"Mistake in line: "+str(line)+ ";          "+current_line+"\n"
+                current_line = self.file.readline()
+                line = line + 1
+            return(errors)
+        else:
+            return 0
+
+    def change_line(self, line_number):
+        '''Changes the current line to the one passed, if file not found returns 0'''
+        if (self.file != ""):
+            self.file.seek(line_number)
+        else:
+            return 0
+    def read_line(self):
+        '''returns the first line of the file, if it had already started,\
+            it returns the next one. If the file is not open itll return 0 '''
+        #chequeamos existencia del archivo
+        if (self.file != ""):
+            current_line = self.file.readline()
+            return(current_line)
+        else:
+            return 0
+    def free_reader(self):
+        '''Cierra la referencia del archivo'''
+        self.file.close()
+        self.file = ""
+    def change_file(self, file_name):
+        '''changes the reader's file, leaves the previous file and \
+            returns 0 if file not found, 1 for operation succesful'''
+        if (file_exist(file_name)):
+            self.file = open(file_name)
+            return 1
+        else:
+            self.file = ""
+            return 0
+print("\n\nlista de patatas: \n\n")
+card_reader = CardReader()
+print(card_reader.check_code())
+
+
+
