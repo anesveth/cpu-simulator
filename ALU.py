a6afb9f4ca0b4110ebd611d25a3de749de563b0f
Ian <ianjenatz@ufm.edu>
2019-08-17 21:37:47 -0600

    Functional ALU, added binary, functional classes and tests

--- ALU.py
+++ ALU.py
@@ -1,89 +1,92 @@
 from IC import IC
 
-command = 0
-sum1 = 0
-carry = 0
-class ALU(IC):
-    def __init__(self):
-#---------------------------------flags-------------------------
-        def zero(a, b):
-            if a == 0 and b == 0:
-                self.zero = TRUE
-        def ovfl(a, b):
-            if a + b > 15:
-                self.ovfl = TRUE
-        def neg(a, b):
-            if a - b < 0:
-                self.neg = TRUE
-#---------------------------------arithmetic--------------------
-    if command == 1:
-        def add(a, b):
-            sum1 = a + b
-            if sum1 >= 2:
-                carry = 1
-    elif command == 2:
-        def addC(a, b, carry):
-            sum1 = a + b + carry
-            if sum1 >= 2:
-                carry = 1
-    elif command == 3:
-        def sub(a, b):
-            sub1 = a - b
-            if sub1 >= 2:
-                carry = 1
-    elif command == 4:
-        def subC(a, b, carry):
-            sub1 = a - b - carry
-            if sub1 >= 2:
-                carry = 1
-    elif command == 5:
-        def onecomp(): #No entendi q significa esto o para q se usa / es algo asi pero saber
-            0 == "1111"
-            1 == "1110"
-            2 == "1101"
-            3 == "1100"
-            4 == "1011"
-            5 == "1010"
-            6 == "1001"
-            7 == "1000"
-            8 == "0111"
-            9 == "0110"
-            10 == "0101"
-            11 == "0100"
-            12 == "0011"
-            13 == "0010"
-            14 == "0001"
-            15 == "0000"
-    elif command == 6:
-        def twocomp(): #No entendi q significa esto o para q se usa / es algo asi pero saber
-            0 == "0000"
-            1 == "0001"
-            2 == "0010"
-            3 == "0011"
-            4 == "0100"
-            5 == "0101"
-            6 == "0110"
-            7 == "0111"
-            8 == "1000"
-            9 == "1001"
-            10 == "1010"
-            11 == "1011"
-            12 == "1100"
-            13 == "1101"
-            14 == "1110"
-            15 == "1111"
-#faltan los twocomp negativos
+'''Test variables'''
+def test():
+    val1 = 10
+    return val1
 
-    elif command == 7:
-        if a == 1 and b == 1:
-            and1 = TRUE
-    elif command == 8:
-        if a == 1 and b == 1 or a == 1 or b == 1:
-            or1 = TRUE
-    elif command == 9: #Todavia tengo que investigar esto pero es algo asi
-        a << b
-        a >> b
-        a & b
-        a | b
-        ~ a
-        a ^ b
+def test2():
+    val2 = 2
+    return val2
+
+def test3():
+    com = 1
+    return com
+
+def test4():
+    signed = False
+    return signed
+
+'''Calling Tests''' #no necesario pero me ayuda a entender la logica mejor.
+y = test()
+print("First value:", y)
+z = test2()
+print("Second value:", z)
+command = test3()
+sm = test4()
+
+'''Class ALU'''
+class ALU(IC):
+    def sum1(self):
+        result = y + z
+        return result
+    def sub(self):
+        result = y - z
+        return result
+    def overf(self):
+        of = False
+        if y > 0 and z > 0 and (x.sum1() < 0 or x.sum1() > 7) or y < 0 and z < 0 and (x.sum1() > 0 or x.sum1() < -7):
+            of = True
+        elif y > 0 and z > 0 and (x.sub() < 0 or x.sub() > 7) or y < 0 and z < 0 and (x.sub() > 0 or x.sub() < -7):
+            of = True
+        return of
+    def carryf(self):
+        cf = False
+        if x.sum1() > 15 or x.sum1() <= 0:
+            cf = True
+        elif x.sub() > 15 or x.sub() <= 0:
+            cf = True
+        return cf
+    def errorf(self):
+        ef = False
+        if sm == False:
+            if x.carryf() == True:
+                ef = False
+        else:
+            if x.overf() == True:
+                ef = True
+        return ef
+    def and1(self):
+        if y == True and z == True:
+            return True
+        else:
+            return False
+    def or1(self):
+        if y == True:
+            return True
+        elif z == True:
+            return True
+        else:
+            return False
+    def not1(self):
+        if y == True:
+            return False
+        else:
+            return True
+
+'''Print of Class ALU'''
+x = ALU()
+if sm == False:
+    if x.sum1() <= 15 and x.sum1() >= -15 and command == 0:
+        print('{0:b}'.format(x.sum1()))
+    if x.sub() <= 15 and x.sub() >= -15 and command == 1:
+        print('{0:b}'.format(x.sub()))
+else:
+    if x.sum1() <= 7 and x.sum1() >= -7 and command == 0:
+        print('{0:b}'.format(x.sum1()))
+    if x.sub() <= 7 and x.sub() >= -7 and command == 1:
+        print('{0:b}'.format(x.sub()))
+
+print("Overflow Flag:", x.overf())
+print("Carry Flag:", x.carryf())
+print("Error Flag:", x.errorf())
