926adf12696e25e2bf36927ef2f738968ebc7af0
Ian <ianjenatz@ufm.edu>
2019-08-25 18:25:12 -0600

    Fixed small binary issue

--- ALU.py
+++ ALU.py
@@ -1,16 +1,37 @@
 from IC import IC
 
 '''Class ALU'''
-class ALU(IC): #Al ALU no le importa si es signed math o no, sin importar el signo es la misma mate pero los flags van a etsar raros.
+class ALU(IC): #Al ALU no le importa si es signed math o no, maximo es 7.
 #-------------------------------------------------------------------------------Arithmetic-------------------------------------------------------------------------
     def sum1(self, data1, data2, signed):
-        signed
+        data1_int = int(data1, 2)
+        data2_int = int(data2, 2)
+        if signed == 1:
+            data1_list = list(data1)
+            data1 = ''.join('1' if x == '0' else '0' for x in data1)
+            data1_list = list(data1)
+            if data1_list[0] == "0":
+                data1_list[0] = "1"
+            else:
+                data1_list[0] = "0"
+            data1 = ''.join(data1_list)
+            data1_int = -int(data1, 2)
+        if signed == 1:
+            data2_list = list(data2)
+            data2 = ''.join('1' if x == '0' else '0' for x in data2)
+            data2_list = list(data2)
+            if data2_list[0] == "0":
+                data2_list[0] = "1"
+            else:
+                data2_list[0] = "0"
+            data2 = ''.join(data2_list)
+            data2_int = -int(data2, 2)
         #sumamos
-        result = int(data1, 2) + int(data2, 2)
+        result = data1_int + data2_int
         #comprobamos overflow, carry y zero
-        overfl_flag = self.overfl(result, signed, data1, data2)
+        overfl_flag = self.overfl(result, data1, data2, data1_int, data2_int)
         carry_flag = self.carry(result)
-        error_flag = self.error(result, signed, data1, data2)
+        error_flag = self.error(result)
         zero_flag = self.zero(result)
         neg_flag = self.negative(result)
         #convertimos el resultado en binario
@@ -28,13 +49,34 @@ class ALU(IC): #Al ALU no le importa si es signed math o no, sin importar el sig
         #llamamos la funcion output como resultado
         return result, overfl_flag, carry_flag, error_flag, zero_flag, neg_flag 
     def sub(self, data1, data2, signed):
-        signed
+        data1_int = int(data1, 2)
+        data2_int = int(data2, 2)
+        if signed == 1:
+            data1_list = list(data1)
+            data1 = ''.join('1' if x == '0' else '0' for x in data1)
+            data1_list = list(data1)
+            if data1_list[0] == "0":
+                data1_list[0] = "1"
+            else:
+                data1_list[0] = "0"
+            data1 = ''.join(data1_list)
+            data1_int = -int(data1, 2)
+        if signed == 1:
+            data2_list = list(data2)
+            data2 = ''.join('1' if x == '0' else '0' for x in data2)
+            data2_list = list(data2)
+            if data2_list[0] == "0":
+                data2_list[0] = "1"
+            else:
+                data2_list[0] = "0"
+            data2 = ''.join(data2_list)
+            data2_int = -int(data2, 2)
         #restamos
-        result = int(data1, 2) - int(data2, 2)
+        result = data1_int - data2_int
         #comprobamos overflow, carry y zero
-        overfl_flag = self.overfl(result, signed, data1, data2)
+        overfl_flag = self.overfl(result, data1, data2, data1_int, data2_int)
         carry_flag = self.carry(result)
-        error_flag = self.error(result, signed, data1, data2)
+        error_flag = self.error(result)
         zero_flag = self.zero(result)
         neg_flag = self.negative(result)
         #convertimos el resultado en binario
@@ -53,48 +95,31 @@ class ALU(IC): #Al ALU no le importa si es signed math o no, sin importar el sig
         return result, overfl_flag, carry_flag, error_flag, zero_flag, neg_flag
 
 #----------------------------------------------------------------------Flags---------------------------------------------------------------------------------------
-    def overfl(self, resultado, signed, data1, data2):
-        #revisa si es signed math
-        if signed == 1: #En signed math, el valor mas grande es 7 y el menor es -7, si es mayor a 15 regresa a 0, igual con -15.
-            if int(data1, 2) > 7 and int(data2, 2) > 7 and (resultado < 7 or resultado > 15) or int(data1, 2) < 7 and int(data1, 2) < 7 and (resultado > 7 or resultado < 15):
-                return True
-            elif int(data1, 2) > 0 and int(data2, 2) > 0 and (resultado < 0 or resultado > 7) or int(data1, 2) < 0 and int(data1, 2) < 0 and (resultado > 0 or resultado < -7):
-                return True
-            else: 
-                return False
-        #Cuando no es signed math
-        else:
-            if resultado > 15 or resultado < 0:
-                return True
-            else:
-                return False
+    def overfl(self, resultado, data1, data2, data1_int, data2_int): #En signed math, el valor mas grande es 7 y el menor es -7.
+        if data1_int > 7 and data2_int > 7 and (resultado < 7 or resultado > 15) or data1_int < 7 and data2_int < 7 and (resultado > 7 or resultado < 15):
+            return True
+        elif data1_int > 0 and data2_int > 0 and (resultado < 0 or resultado > 7) or data1_int < 0 and data2_int < 0 and (resultado > 0 or resultado < -7):
+            return True
+        else: 
+            return False
     def carry(self, resultado): #carry flag
         #el carry flag solo se prende si el primer bit cambia de valor, no le importa si es signed o no.
-        if resultado > 15 or resultado <= 0:
+        if resultado > 7 or resultado <= 0:
             return True
         else:
             return False
-    def error(self, result, signed, data1, data2): #error flag (If signed math, error only when carry flag. If not sm, error only when overflow flag)
-        #Si es signed math, hay error si se prende el carry flag.
-        overfl_flag = self.overfl(result, signed, data1, data2)
+    def error(self, result): #error flag, si es signed math, hay error si se prende el carry flag.
         carry_flag = self.carry(result)
-        if signed == 1:
-            if carry_flag == True:
-                return True
-            else:
-                return False
-        #Si no es signed math, hay error si se prende el overflow flag.
+        if carry_flag == True:
+            return True
         else:
-            if overfl_flag == True:
-                return True
-            else: 
-                return False
+            return False
     def zero(self, resultado): #Checks if the result is 0.
         if resultado == 0:
             return True
         else:
             return False
-    def negative(self, result): #Checks if the result is negative. (Can only happen if it is signed math)
+    def negative(self, result): #Checks if the result is negative.
         if result < 0:
             return True
         else:
@@ -124,7 +149,7 @@ mi_alu = ALU()
 print("Sum test:", mi_alu.sum1("0011", "0110", 0))
 print("Sum test 2:", mi_alu.sum1("1101", "1110", 1))
 
-print("Sub test:", mi_alu.sub("0011", "1100", 0))
+print("Sub test:", mi_alu.sub("0011", "1100", 1))
 
 print(mi_alu.and1("11", "11"))
 
