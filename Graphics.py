aad7ce18246562589b801305d3b7e18f439055c3
anesveth <annesveth@ufm.edu>
2019-08-25 20:49:11 -0600

    changed .code text display size

--- Graphics.py
+++ Graphics.py
@@ -132,24 +132,25 @@ class graphics(IC):
             codetxt="123456789101112131415161718192021222324252627282930313233343536373839404142434445464748495051525354555657585960"
             lenght_code=len(codetxt)
             line_start=0#position in string
-            if lenght_code>34:
+            if lenght_code>45:
                 line_space=120#position in display
-                finish_length=34
+                finish_length=45
                 lenght_code_left=lenght_code
-                while lenght_code_left>34: #stops the string displayed from going over the 'Instructios.code' window
+                while lenght_code_left>45: #stops the string displayed from going over the 'Instructios.code' window
                     displayedtxt=codetxt[line_start:finish_length]
-                    codetxt_displayed=self.put_text("{}".format(displayedtxt),x=69,y=line_space,size=45)
+                    codetxt_displayed=self.put_text("{}".format(displayedtxt),x=69,y=line_space,size=35)
                     print(codetxt_displayed)
                     line_space+=40
-                    line_start+=34
-                    finish_length=line_start+34
-                    lenght_code_left=lenght_code_left-34
+                    line_start+=45
+                    finish_length=line_start+45
+                    lenght_code_left=lenght_code_left-45
                 displayedtxt=str(codetxt[line_start:])
-                codetxt_displayed=self.put_text("{}".format(displayedtxt),x=69,y=line_space,size=45)
+                codetxt_displayed=self.put_text("{}".format(displayedtxt),x=69,y=line_space,size=35)
                 print(codetxt_displayed)
             #if it's not necessary (because the string lenght won't go over the window)
-            codetxt_displayed=self.put_text("{}".format(codetxt[0:34]),x=69,y=120,size=45)
-            print(codetxt_displayed)
+            else:
+                codetxt_displayed=self.put_text("{}".format(codetxt[0:45]),x=69,y=120,size=35)
+                print(codetxt_displayed)
 
 
             for event in pygame.event.get():
