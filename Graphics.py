8561a4e52e34f28a0209a3cb4f5630b661f506f4
anesveth <annesveth@ufm.edu>
2019-08-25 19:01:12 -0600

    Placed all locations in display window, currently only showcases placeholders

--- Graphics.py
+++ Graphics.py
@@ -3,21 +3,16 @@ import pygame
 class graphics(IC):
     def __init__(self):
         pygame.init() 
-        # define for white colour 
-        self.white = (255, 255, 255) 
+        self.white = (255, 255, 255)# white background
         # assigning values to X and Y variable 
-        X = 1920
-        Y = 1080
+        width = 2000
+        height = 1435
         # create the display surface (display window) 
-        self.display_surface = pygame.display.set_mode((X, Y ), pygame.RESIZABLE) 
-        # set the window name 
-        pygame.display.set_caption('IC simulation') 
+        self.display_surface = pygame.display.set_mode((width, height ), pygame.RESIZABLE) 
+        pygame.display.set_caption('IC simulation')# window name 
         # create a surface object, with the image 
         self.image = pygame.image.load(r'display.png') 
-        #creates font to use
-        self.font = pygame.font.SysFont("comicsansms", 72)
-    def put_text(self, text, x=0, y=0, color=(0,0,0), 
-        font_type = 'OpenSans-Regular.ttf', size = 30):
+    def put_text(self, text, x=0, y=0, color=(0,0,0),font_type = 'Arial.ttf', size = 40):
         '''displays a text on surface
         returns surface, 0 if failed'''
         try:
@@ -25,7 +20,8 @@ class graphics(IC):
             text = font.render(str(text), True, color)
             self.display_surface.blit(text, (x, y))
             return text
-        except Exception:
+        except Exception as e:
+            print (str(e))
             return 0
     def put_image(self, object1, x, y):
         # copying the object to display surface object at x.y
@@ -38,12 +34,104 @@ class graphics(IC):
             # completely fill the surface object 
             # with white colour 
             self.display_surface.fill(self.white)   
-            self.put_image(self.image, 100, 100)
-            #displaying text
-            teext = self.put_text("alv")
-            self.display_surface.blit(teext, (0,0)) 
-            # iterate over the list of Event objects 
-            # that was returned by pygame.event.get() method. 
+            self.put_image(self.image, 0, 0)
+            #DISPLAYING TEXT
+            clocktxt = self.put_text("1.2 Hz",x=1740,y=67,size=60)
+            print (clocktxt) 
+            ############################################## RAM (69 downward separation)
+            ################### OPCODE SECTION
+            opcode0txt=self.put_text("0000",x=1505,y=296,size=48)
+            print (opcode0txt) 
+            opcode1txt=self.put_text("0001",x=1505,y=365,size=48)
+            print (opcode1txt) 
+            opcode2txt=self.put_text("0010",x=1505,y=434,size=48)
+            print (opcode2txt) 
+            opcode3txt=self.put_text("0011",x=1505,y=503,size=48)
+            print (opcode3txt) 
+            opcode4txt=self.put_text("0100",x=1505,y=572,size=48)
+            print (opcode4txt) 
+            opcode5txt=self.put_text("0101",x=1505,y=641,size=48)
+            print (opcode5txt) 
+            opcode6txt=self.put_text("0110",x=1505,y=710,size=48)
+            print (opcode6txt) 
+            opcode7txt=self.put_text("0111",x=1505,y=779,size=48)
+            print (opcode7txt) 
+            opcode8txt=self.put_text("1000",x=1505,y=848,size=48)
+            print (opcode8txt) 
+            opcode9txt=self.put_text("1001",x=1505,y=917,size=48)
+            print (opcode9txt) 
+            opcode10txt=self.put_text("1010",x=1505,y=986,size=48)
+            print (opcode10txt) 
+            opcode11txt=self.put_text("1011",x=1505,y=1055,size=48)
+            print (opcode11txt) 
+            opcode12txt=self.put_text("1100",x=1505,y=1124,size=48)
+            print (opcode12txt) 
+            opcode13txt=self.put_text("1101",x=1505,y=1193,size=48)
+            print (opcode13txt) 
+            opcode14txt=self.put_text("1110",x=1505,y=1262,size=48)
+            print (opcode14txt) 
+            opcode15txt=self.put_text("1111",x=1505,y=1331,size=48)
+            print (opcode15txt) 
+            #
+            ################### NUMBER SECTION
+            #
+            number0txt=self.put_text("0000",x=1765,y=296,size=48)
+            print (number0txt) 
+            number1txt=self.put_text("0001",x=1765,y=365,size=48)
+            print (number1txt) 
+            number2txt=self.put_text("0010",x=1765,y=434,size=48)
+            print (number2txt) 
+            number3txt=self.put_text("0011",x=1765,y=503,size=48)
+            print (number3txt) 
+            number4txt=self.put_text("0100",x=1765,y=572,size=48)
+            print (number4txt) 
+            number5txt=self.put_text("0101",x=1765,y=641,size=48)
+            print (number5txt) 
+            number6txt=self.put_text("0110",x=1765,y=710,size=48)
+            print (number6txt) 
+            number7txt=self.put_text("0111",x=1765,y=779,size=48)
+            print (number7txt) 
+            number8txt=self.put_text("1000",x=1765,y=848,size=48)
+            print (number8txt) 
+            number9txt=self.put_text("1001",x=1765,y=917,size=48)
+            print (number9txt) 
+            number10txt=self.put_text("1010",x=1765,y=986,size=48)
+            print (number10txt) 
+            number11txt=self.put_text("1011",x=1765,y=1055,size=48)
+            print (number11txt) 
+            number12txt=self.put_text("1100",x=1765,y=1124,size=48)
+            print (number12txt) 
+            number13txt=self.put_text("1101",x=1765,y=1193,size=48)
+            print (number13txt) 
+            number14txt=self.put_text("1110",x=1765,y=1262,size=48)
+            print (number14txt) 
+            number15txt=self.put_text("1111",x=1765,y=1331,size=48)
+            print (number15txt)
+            ############################################## ALU 
+            alutxt=self.put_text("0001-0001",x=1065,y=525,size=48)
+            print (alutxt)
+            flagtxt=self.put_text("Z",x=1145,y=642,size=55)
+            print (flagtxt)
+            ############################################## REGISTERS 
+            outputtxt=self.put_text("0000",x=1000,y=860,size=50) #OUTPUT R
+            print (outputtxt)
+            instruction_registertxt=self.put_text("0001 0001",x=950,y=1099,size=50) #INSTRUCTION R
+            print (instruction_registertxt)
+            instruction_adress_registertxt=self.put_text("0010 0001",x=950,y=1299,size=50) #INSTRUCTION ADRESS R
+            print (instruction_adress_registertxt)
+            registerAtxt=self.put_text("0000 0000",x=198,y=697,size=49) #REGISTER A
+            print (registerAtxt)
+            registerBtxt=self.put_text("0000 0000",x=198,y=888,size=49) #REGISTER B
+            print (registerBtxt)
+            registerCtxt=self.put_text("0000 0000",x=198,y=1100,size=49) #REGISTER C
+            print (registerCtxt)
+            registerDtxt=self.put_text("0000 0000",x=198,y=1309,size=49) #REGISTER D
+            print (registerDtxt)
+            ############################################## .CODE FILE DISPLAY 
+            codetxt=self.put_text("{} width".format(len("1234567891011121314151617181920212")),x=69,y=120,size=45)
+            print(codetxt)
+
+
             for event in pygame.event.get():
                 print(str(event)) 
                 if event.type == pygame.VIDEORESIZE:
